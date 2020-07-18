44fdb5c92b746da21debfa914188753a0c078c77
MoviesHuntHD <68388096+MoviesHuntHD@users.noreply.github.com>
2020-07-18 09:37:38 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -33,12 +33,12 @@ async def new_join_f(client, message):
 
 
 async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
+    # await message.reply_text("Read Pinned Message, Don't Ping Admins.", quote=True)
     #channel_id = str(AUTH_CHANNEL)[4:]
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""No one gonna HELP here!! Leech if you KNOW :)</a>""", disable_web_page_preview=True)
+    await message.reply_text(""" Read Pinned Message, And Join @MoviesHunt_HD </a>""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
